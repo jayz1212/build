6898915baba5ea16a369704b903ba8c0c2c2f46a
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2025-02-09 13:18:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -70,6 +70,8 @@ rm -rf .repo/local_manifests/
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --depth=1
 git clone https://github.com/jayz1212/local.git --depth 1 -b main .repo/local_manifests
 /opt/crave/resync.sh 
+/opt/crave/resync.sh 
+/opt/crave/resync.sh 
 source build/envsetup.sh 
 make clean
 echo $RBE_DIR
