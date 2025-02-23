6f651954dc4be41b2466b31d86b3ee9d82765a02
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2025-02-24 06:21:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
-
-
-
-
-
 rm -rf .repo/local_manifests/ 
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --depth 1
 git clone https://github.com/jayz1212/local.git --depth 1 -b main .repo/local_manifests
