4dff7a2e38cebc0834e2951fcbccee249a6831b3
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2025-02-24 12:01:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
-rm -rf .repo/local_manifests/ 
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs --depth 1
-git clone https://github.com/jayz1212/local.git --depth 1 -b main .repo/local_manifests
-/opt/crave/resync.sh 
-source build/envsetup.sh 
-make installclean
-brunch vayu
+rm -rf .repo/local_manifests
+rm -rf hardware/xiaomi
+rm -rf device/xiaomi
+rm -rf vendor/xiaomi
+rm -rf vendor/lineage-priv
+rm -rf kernel/xiaomi
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+git clone https://github.com/vayu-development-sources/local_manifests.git -b evo15-dolby .repo/local_manifests
+/opt/crave/resync.sh
+git clone https://gitlab.com/ArmSM/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
 
-
-
-
-
-
-
+cd frameworks/base/
+git fetch https://github.com/xc112lg/android_frameworks_base.git patch-2
+git cherry-pick 3a3b3718ffcfe53127cbfa228577f02d825e1960
+cd -
+. build/envsetup.sh && lunch lineage_vayu-ap4a-user && m evolution
