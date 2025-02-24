1c9d49f4922ed77903b8eb83874163e3a9371ac6
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2025-02-24 09:39:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf .repo/local_manifests/ 
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --depth 1
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs --depth 1
 git clone https://github.com/jayz1212/local.git --depth 1 -b main .repo/local_manifests
 /opt/crave/resync.sh 
 source build/envsetup.sh 
