89e086c03f53d1a866f1570884e028cb5cc046a7
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-02 10:54:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,8 @@ git clone https://github.com/jayz1212/local.git -b main .repo/local_manifests
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh
 
-
+sed -i 's|PRODUCT_AAPT_CONFIG := normal hdpi xhdpi|PRODUCT_AAPT_CONFIG ?= normal hdpi xhdpi|' device/samsung/a5-common/BoardConfigCommon.mk
+sed -i 's|PRODUCT_AAPT_PREF_CONFIG := xhdpi|PRODUCT_AAPT_PREF_CONFIG ?= xhdpi|' device/samsung/a5-common/BoardConfigCommon.mk
 . build/envsetup.sh
 brunch a5ltechn
 
