ee224647adb5215ca8710e24b1022da93023b73a
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-03 21:13:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -66,9 +66,9 @@ fi
 
 echo "[✓] Done fixing ril_vendor.h include!"
 
-curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/ril.sh | bash
+#curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/ril.sh | bash
 
-
+curl -sf https://raw.githubusercontent.com/jayz1212/build/9a6c2c28a90826fd37f5653d4839ebda946fd606/ril.sh | bash
 make installclean
 brunch a5ltechn 2>&1 | tee build.log
 
