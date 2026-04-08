04d819e6342b06cde895ca16cc5dfee9c705d89a
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-08 17:29:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -83,14 +83,14 @@ export BUILD_BROKEN_MISSING_API_CHECKS=true
 # scripts/config --disable CC_STACKPROTECTOR_REGULAR
 
 
-echo 'LOCAL_SRC_FILES := $(filter-out %/sap/%, $(LOCAL_SRC_FILES))' >> packages/apps/Bluetooth/Android.mk
+
 
 curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/compilerfix.sh | bash
 
 curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/fixlib.sh | bash
 curl -sf https://raw.githubusercontent.com/jayz1212/build/1448334332344b605fdb4a6dadf5a3beece4883d/java.sh | bash
 curl -sf https://raw.githubusercontent.com/jayz1212/build/a4bdf55c6e6584bb670d90596ad46d2f9f8edb33/fixcurs.sh | bash
-
+curl -sf https://raw.githubusercontent.com/jayz1212/build/4ff76f942afb63b356034ad5e4068bb41d7781c8/fixsap.sh | bash
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk && \
 export PATH=$JAVA_HOME/bin:$PATH
