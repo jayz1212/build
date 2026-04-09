3d60ab7bd96263e200b0ed774d4132bbf0e01484
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-09 15:28:27 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -2,12 +2,16 @@
 
 #!/usr/bin/env bash
 
-#
+# export PYTHON=python3.10
+# export PYTHON=python3
+sudo rm -rf src/android
 
 rm -rf .repo/local_manifests
 rm -rf device/samsung
 rm -rf vendor/samsung
 rm -rf kernel/samsung
+rm -rf packages/apps/Bluetooth
+rm -rf *.tar.gz
 ##https://github.com/accupara/los-cm14.1.git -b cm-14.1 --depth=1 --git-lfs
 #repo init -u https://github.com/accupara/los16.git -b lineage-16.0 --depth=1 --git-lfs
 repo init -u https://github.com/LineageOS/android.git -b lineage-17.1 --depth=1 --git-lfs
@@ -21,7 +25,10 @@ sed -i 's|PRODUCT_AAPT_PREF_CONFIG := xhdpi|PRODUCT_AAPT_PREF_CONFIG ?= xhdpi|' 
 . build/envsetup.sh
 
 
+
 curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/ril.sh | bash
+
+
 # export ARCH=arm
 # export CC=clang
 # export LD=ld.lld
@@ -37,11 +44,18 @@ curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/ril.sh
 # export PATH_OVERRIDE_SOONG="prebuilts/build-tools/path/linux-x86/path_override"
 # export SUBARCH=arm
 # export CROSS_COMPILE=/tmp/src/android/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+
+
+# # 1. Force correct Java (VERY IMPORTANT)
 # export JAVA_HOME=$(pwd)/prebuilts/jdk/jdk11/linux-x86
 # export PATH=$JAVA_HOME/bin:$PATH
-# rm -rf out/soong/.intermediates/libcore
-# export RELAX_USES_LIBRARY_CHECK=true
-# export BUILD_BROKEN_MISSING_API_CHECKS=true
+
+# 2. Clean only the broken module
+rm -rf out/soong/.intermediates/libcore
+
+# 3. Disable strict metalava checks (safe for msm8916)
+export RELAX_USES_LIBRARY_CHECK=true
+export BUILD_BROKEN_MISSING_API_CHECKS=true
 
 # 4. Rebuild
 
@@ -50,8 +64,43 @@ curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/ril.sh
 # scripts/config --disable CC_STACKPROTECTOR_STRONG
 # scripts/config --disable CC_STACKPROTECTOR_REGULAR
 
-. build/envsetup.sh
-brunch a5ltechn 2>&1 | tee build.log
 
-# Upload to ix.ioe
-curl -F "file=@build.log" https://temp.sh/upload
+
+
+source <(curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/compilerfix.sh)
+
+# curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/fixlib.sh | bash
+
+source <(curl -sf https://raw.githubusercontent.com/jayz1212/build/a4bdf55c6e6584bb670d90596ad46d2f9f8edb33/fixcurs.sh | bash)
+source <(curl -sf https://raw.githubusercontent.com/jayz1212/build/4ff76f942afb63b356034ad5e4068bb41d7781c8/fixsap.sh | bash)
+#source <(curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/java2.sh | bash)
+
+
+. build/envsetup.sh
+lunch lineage_a5ltechn-userdebug
+echo "☕ Forcing Java 8 (hard override)..."
+
+# JDK8="/usr/lib/jvm/java-8-openjdk"
+
+# export JAVA_HOME="$JDK8"
+# export PATH="$JAVA_HOME/bin:$PATH"
+
+# 🔥 HARD FIX
+# ln -sf $JDK8/bin/java prebuilts/jdk/jdk9/linux-x86/bin/java
+
+# hash -r
+
+# which java
+# java -version
+
+# CLEAN AFTER FIX
+rm -rf out/soong/.intermediates
+rm -rf out/soong/.intermediates/frameworks/base
+rm -rf out/soong/.intermediates/libcore
+rm -rf out/soong/.intermediates/frameworks/base/api*
+# build
+make clean
+#m Bluetooth -j4 2>&1 | tee build.log && curl -F "file=@build.log" https://temp.sh/upload
+make framework -j8 2>&1 | tee build.log && curl -F "file=@build.log" https://temp.sh/upload
+
+java -version
