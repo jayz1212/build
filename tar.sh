11db4db76dae50e25192bf3e3e73999a59578c9d
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-11 17:10:07 +0800

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -7,19 +7,11 @@ cd /tmp/src/android/out/target/product/a5ltechn
 
 tar -cvf recovery.tar recovery.img
 
-# Add md5 checksum
-md5sum -t recovery.tar >> recovery.tar
-mv recovery.tar recovery.tar.md5
-
-# Or one-liner
-tar -cvf recovery.tar recovery.img && md5sum -t recovery.tar >> recovery.tar && mv recovery.tar recovery.tar.md5
 
 
-
-
-echo "Created: recovery.tar.md5"
+echo "Created: recovery.tar"
 cd -
-cp out/target/product/a5ltechn/recovery.tar.md5 crdroid10.x
+cp out/target/product/a5ltechn/recovery.tar crdroid10.x
 cd crdroid10.x
 chmod +x multi_upload.sh
 ./multi_upload.sh
