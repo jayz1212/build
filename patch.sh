bf23221a2e020e361d033571e183622820de4825
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-11 10:44:53 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,49 +1,49 @@
-#!/usr/bin/env python3
-#
-# Python3-safe version of check_radio_versions.py
-#
+#!/bin/bash
+# setup_jdk8_debian13.sh
 
-import os
-import sys
+set -e
 
-def check_file(fn, key, allowed):
-    try:
-        with open(fn + ".sha1", "r") as f:
-            version = f.read().strip()
-    except IOError:
-        print("*** Error opening \"%s.sha1\"; can't verify %s" % (fn, key))
-        return False
+echo "Setting up JDK 8 on Debian 13 (Trixie)..."
 
-    if not any(v in allowed for v in [version]):
-        print("*** \"%s\" is version %s; not any %s allowed by \"%s\"." %
-              (fn, version, allowed, key))
-        return False
+# Method 2: Install from Adoptium (Recommended)
+echo "Downloading JDK 8 from Adoptium..."
+cd /tmp
+wget -q --show-progress https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u432-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u432b06.tar.gz
 
-    return True
+echo "Extracting to /opt/jdk8..."
+sudo tar -xzf OpenJDK8U-jdk_x64_linux_hotspot_8u432b06.tar.gz -C /opt/
+sudo mv /opt/jdk8u432-b06 /opt/jdk8
 
+# Remove old alternatives if they exist
+sudo update-alternatives --remove-all java 2>/dev/null || true
+sudo update-alternatives --remove-all javac 2>/dev/null || true
 
-def main():
-    # If no args, just exit clean (TWRP doesn't really need this)
-    if len(sys.argv) < 2:
-        return 0
+# Set up alternatives
+echo "Setting up alternatives..."
+sudo update-alternatives --install /usr/bin/java java /opt/jdk8/bin/java 100
+sudo update-alternatives --install /usr/bin/javac javac /opt/jdk8/bin/javac 100
+sudo update-alternatives --install /usr/bin/jar jar /opt/jdk8/bin/jar 100
+sudo update-alternatives --install /usr/bin/javadoc javadoc /opt/jdk8/bin/javadoc 100
 
-    success = True
+# Set JDK 8 as default
+sudo update-alternatives --set java /opt/jdk8/bin/java
+sudo update-alternatives --set javac /opt/jdk8/bin/javac
 
-    for arg in sys.argv[1:]:
-        parts = arg.split("=")
-        if len(parts) != 2:
-            continue
+# Set JAVA_HOME
+echo "export JAVA_HOME=/opt/jdk8" >> ~/.bashrc
+echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
 
-        key = parts[0]
-        allowed = parts[1].split(",")
+# Clean up
+rm /tmp/OpenJDK8U-jdk_x64_linux_hotspot_8u432b06.tar.gz
 
-        fn = os.path.join("radio", key)
+# Source and verify
+source ~/.bashrc
 
-        if not check_file(fn, key, allowed):
-            success = False
+echo ""
+echo "Verifying installation..."
+java -version
+javac -version
 
-    return 0 if success else 1
-
-
-if __name__ == "__main__":
-    sys.exit(main())
+echo ""
+echo "JAVA_HOME is set to: $JAVA_HOME"
+echo "JDK 8 installation complete!"
