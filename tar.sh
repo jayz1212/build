0b1d058fff14f6441c187a7cb8524f306c8a1050
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-16 12:06:11 +0800

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -13,7 +13,7 @@ git clone https://$GH_TOKEN@github.com//jayz1212/crdroid10.x
 #cd -
 rm -rf crdroid10.x/*.img crdroid10.x/*.zip
 cp out/target/product/*/recovery.img crdroid10.x
-e
+cp out/target/product/*/*.zip crdroid10.x
 
 cd crdroid10.x
 chmod +x multi_upload.sh
