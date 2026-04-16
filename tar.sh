ab6c6af25ace0071159dc34ce42230395d6cbf2f
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-16 20:27:39 +0800

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -1,20 +1,20 @@
 export GH_TOKEN=$(cat gh_token.txt)
 git clone https://$GH_TOKEN@github.com//jayz1212/crdroid10.x
-#cd /tmp/src/android/out/target/product/a5ltechn
+cd /tmp/src/android/out/target/product/a5ltechn
 
 # Create tar.md5 for Odin
 
 
-#tar -cvf recovery.tar recovery.img
+tar -cvf recovery.tar recovery.img
 
 
 
-#echo "Created: recovery.tar"
-#cd -
-rm -rf crdroid10.x/*.img crdroid10.x/*.zip
+echo "Created: recovery.tar"
+cd -
+rm -rf crdroid10.x/*.img crdroid10.x/*.zip crdroid10.x/*.tar
 cp out/target/product/*/recovery.img crdroid10.x
 cp out/target/product/*/*.zip crdroid10.x
-
+cp out/target/product/*/*.tar crdroid10.x
 cd crdroid10.x
 chmod +x multi_upload.sh
 ./multi_upload.sh
