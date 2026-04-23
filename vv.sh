21f31cd8871cbc70dd868f56e5b382edb372a80c
Rini Aditya Marpaung <160797500+jayz1212@users.noreply.github.com>
2026-04-23 20:43:35 +0800

    Create vv.sh

---
+++ vv.sh
@@ -0,0 +1,84 @@
+sudo apt update
+sudo apt install patchelf -y
+
+rm -rf .repo/local_manifests/
+rm -rf .repo/manifests/
+rm -rf device/xiaomi
+rm -rf device/xiaomi/blossom-kernel
+rm -rf vendor/xiaomi
+rm -rf vendor/xiaomi/miuicamera
+rm -rf hardware/mediatek
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf hardware/dolby
+# rm -rf bootable
+# rm -rf build/make
+# rm -rf frameworks/av
+# rm -rf frameworks/base
+# rm -rf hardware/google/pixel
+# rm -rf hawrdware/interfaces
+# rm -rf packages/modules/Bluetooth
+#rm -rf vendor
+#rm -rf TMP_PATCHES
+
+
+
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth=1 --git-lfs
+git clone https://github.com/jayz1212/local --depth 1 -b cda13 .repo/local_manifests
+repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
+
+
+/opt/crave/resync.sh
+
+
+
+# DEVICE_DIR="device/xiaomi/blossom/sepolicy/vendor"
+# FILE="$DEVICE_DIR/init.te"
+
+# echo "[*] Fixing sepolicy neverallow (mounton)..."
+
+# if [ ! -f "$FILE" ]; then
+#     echo "[!] File not found: $FILE"
+#     exit 1
+# fi
+
+# # Backup
+# cp "$FILE" "$FILE.bak"
+
+# # 1. Remove illegal mounton rules
+# sed -i '/volte_.*_exec.*mounton/d' "$FILE"
+
+# # 2. Add safe rules if not already present
+# grep -q "volte_imcb_exec:file" "$FILE" || cat >> "$FILE" <<EOF
+
+# # Auto-added safe VoLTE rules
+# allow init volte_imcb_exec:file { read open execute getattr };
+# allow init volte_stack_exec:file { read open execute getattr };
+# allow init volte_ua_exec:file { read open execute getattr };
+# EOF
+
+# echo "[✓] mounton rules removed and safe rules added"
+#rm -rf hardware/mediatek/interfaces/hardware/bluetooth
+rg -l -0 '<<<<<<<|=======|>>>>>>>' hardware/mediatek | xargs -0 sed -i '/^<<<<<<< /d;/^=======/d;/^>>>>>>> /d'
+#./device/xiaomi/blossom/applyPatches.sh device/xiaomi/blossom/patches
+source build/envsetup.sh
+
+export TARGET_USES_PICO_GAPPS=true
+export TARGET_ENABLE_BLUR=false
+export WITH_GMS=false
+rm -rf hardware/interfaces/biometrics/fingerprint/2.1/default
+
+sed -i '\|$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)|d' device/xiaomi/blossom/lineage_blossom.mk
+sed -i '/# FM Radio/,+2d' device/xiaomi/blossom/device.mk
+
+export NINJA_ARGS="-j1 -l1"
+export SOONG_UI_JOBS=1
+export GOMAXPROCS=1
+export _JAVA_OPTIONS="-Xmx1g"
+
+
+
+
+
+lunch lineage_blossom-bp4a-eng
+#make clean
+m evolution -j1 2>&1 | tee build1.log && curl -F "file=@build1.log" https://temp.sh/upload
