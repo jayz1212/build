0ee95c43aac328c2c974bbb8f2a88020f7b2e1e8
Reza Setiawan Kurniawan <160797500+jayz1212@users.noreply.github.com>
2026-04-27 21:09:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -109,6 +109,6 @@ PRODUCT_SYSTEM_PROPERTIES += \
 EOF
 
 lunch lineage_blossom-bp4a-eng
-# m clean
+m installclean
 #make clean
 m evolution 2>&1 | tee build1.log && curl -F "file=@build1.log" https://temp.sh/upload
