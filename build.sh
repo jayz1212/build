2c3967027f950f3f1417b7fe4dbe510953e51c1f
Reza Setiawan Kurniawan <160797500+jayz1212@users.noreply.github.com>
2026-04-28 06:00:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -52,8 +52,8 @@ sed -i '/# FM Radio/,+2d' device/xiaomi/blossom/device.mk
 sed -i '/# Besloudness/,+2d' device/xiaomi/blossom/device.mk
 sed -i '/dirty_writeback_centisecs/d' device/mediatek/sepolicy_vndr/basic/non_plat/genfs_contexts
 # sed -i '/system_server.*sys_module/d' device/mediatek/sepolicy_vndr/basic/non_plat/system_server.te
-# sed -i '/^persist.vendor.audio\.\s/d' device/xiaomi/blossom/sepolicy/vendor/property_contexts
-# sed -i '/ro.vendor.audio\./d' device/xiaomi/blossom/sepolicy/*/property_contexts
+sed -i '/^persist.vendor.audio\.\s/d' device/xiaomi/blossom/sepolicy/vendor/property_contexts
+sed -i '/ro.vendor.audio\./d' device/xiaomi/blossom/sepolicy/*/property_contexts
 
 # DEVICE_DIR="device/xiaomi/blossom/sepolicy/vendor"
 # FILE="$DEVICE_DIR/init.te"
