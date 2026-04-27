676974c10fb8a38656226fe261d87b823932269a
Reza Setiawan Kurniawan <160797500+jayz1212@users.noreply.github.com>
2026-04-28 05:01:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -34,32 +34,7 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 
 
 
-DEVICE_DIR="device/xiaomi/blossom/sepolicy/vendor"
-FILE="$DEVICE_DIR/init.te"
 
-echo "[*] Fixing sepolicy neverallow (mounton)..."
-
-if [ ! -f "$FILE" ]; then
-    echo "[!] File not found: $FILE"
-    exit 1
-fi
-
-# Backup
-cp "$FILE" "$FILE.bak"
-
-# 1. Remove illegal mounton rules
-sed -i '/volte_.*_exec.*mounton/d' "$FILE"
-
-# 2. Add safe rules if not already present
-grep -q "volte_imcb_exec:file" "$FILE" || cat >> "$FILE" <<EOF
-
-# Auto-added safe VoLTE rules
-allow init volte_imcb_exec:file { read open execute getattr };
-allow init volte_stack_exec:file { read open execute getattr };
-allow init volte_ua_exec:file { read open execute getattr };
-EOF
-
-echo "[✓] mounton rules removed and safe rules added"
 #rm -rf hardware/mediatek/interfaces/hardware/bluetooth
 rg -l -0 '<<<<<<<|=======|>>>>>>>' hardware/mediatek | xargs -0 sed -i '/^<<<<<<< /d;/^=======/d;/^>>>>>>> /d'
 #./device/xiaomi/blossom/applyPatches.sh device/xiaomi/blossom/patches
@@ -67,17 +42,46 @@ source build/envsetup.sh
 git clone https://github.com/jayz1212/v30 --depth 1 -b main prebuilts/vndk/v30/
 export TARGET_USES_PICO_GAPPS=true
 export TARGET_ENABLE_BLUR=false
-expotr WITH_ADB_INSECURE=true
+export WITH_ADB_INSECURE=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 #export WITH_GMS=false
 rm -rf hardware/interfaces/biometrics/fingerprint/2.1/default
 
 sed -i '\|$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)|d' device/xiaomi/blossom/lineage_blossom.mk
 sed -i '/# FM Radio/,+2d' device/xiaomi/blossom/device.mk
 sed -i '/# Besloudness/,+2d' device/xiaomi/blossom/device.mk
-sed -i '/dirty_writeback_centisecs/d' device/mediatek/sepolicy_vndr/basic/non_plat/genfs_contexts
-sed -i '/system_server.*sys_module/d' device/mediatek/sepolicy_vndr/basic/non_plat/system_server.te
-sed -i '/^persist.vendor.audio\.\s/d' device/xiaomi/blossom/sepolicy/vendor/property_contexts
-sed -i '/ro.vendor.audio\./d' device/xiaomi/blossom/sepolicy/*/property_contexts
+# sed -i '/dirty_writeback_centisecs/d' device/mediatek/sepolicy_vndr/basic/non_plat/genfs_contexts
+# sed -i '/system_server.*sys_module/d' device/mediatek/sepolicy_vndr/basic/non_plat/system_server.te
+# sed -i '/^persist.vendor.audio\.\s/d' device/xiaomi/blossom/sepolicy/vendor/property_contexts
+# sed -i '/ro.vendor.audio\./d' device/xiaomi/blossom/sepolicy/*/property_contexts
+
+# DEVICE_DIR="device/xiaomi/blossom/sepolicy/vendor"
+# FILE="$DEVICE_DIR/init.te"
+
+# echo "[*] Fixing sepolicy neverallow (mounton)..."
+
+# if [ ! -f "$FILE" ]; then
+#     echo "[!] File not found: $FILE"
+#     exit 1
+# fi
+
+# # Backup
+# cp "$FILE" "$FILE.bak"
+
+# # 1. Remove illegal mounton rules
+# sed -i '/volte_.*_exec.*mounton/d' "$FILE"
+
+# # 2. Add safe rules if not already present
+# grep -q "volte_imcb_exec:file" "$FILE" || cat >> "$FILE" <<EOF
+
+# # Auto-added safe VoLTE rules
+# allow init volte_imcb_exec:file { read open execute getattr };
+# allow init volte_stack_exec:file { read open execute getattr };
+# allow init volte_ua_exec:file { read open execute getattr };
+# EOF
+
+# echo "[✓] mounton rules removed and safe rules added"
+
 
 # FILE="packages/apps/Settings/Evolver/src/org/evolution/settings/fragments/miscellaneous/TrickyStoreAppPicker.kt"
 
