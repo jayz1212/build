ace184160330fb36f84fb588c00ca9991f60843e
Lukman Simbolon <160797500+jayz1212@users.noreply.github.com>
2026-05-31 19:09:21 +0800

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -1,3 +1,45 @@
+sudo apt update
+sudo apt install patchelf -y
+sudo apt install ccache -y
+mkdir tmp
+export CCACHE_DIR=tmp
+export USE_CCACHE=1
+ccache -s
+rm -rf .repo/local_manifests/
+rm -rf .repo/manifests/
+rm -rf device/xiaomi
+rm -rf device/xiaomi/blossom-kernel
+rm -rf vendor/xiaomi
+rm -rf vendor/xiaomi/miuicamera
+rm -rf hardware/mediatek
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf TMP_PATCHES
+#repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --depth=1 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth=1 --git-lfs
+git clone https://github.com/jayz1212/local --depth 1 -b cd16 .repo/local_manifests
+repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
+/opt/crave/resync.sh
+#export TARGET_USES_PICO_GAPPS=true
+export TARGET_USES_PICO_GAPPS=true
+export TARGET_ENABLE_BLUR=false
+export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GMS=true
+source build/envsetup.sh
+lunch lineage_blossom-bp4a-eng
+make installclean
+#make clean # one time
+#m bacon
+m evolution
+ccache -s
+curl -sf https://raw.githubusercontent.com/jayz1212/build/refs/heads/main/tar.sh | bash
+
+
+
+
+
+
+
+
 echo "machine gitlab.com" >> ~/.netrc
 echo "login dtiven13" >> ~/.netrc
 export PASS=$(cat pass.txt)
