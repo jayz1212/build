9f9d9b42c9d7ef69efc3916b7795a95cc6f42dd5
Lukman Simbolon <160797500+jayz1212@users.noreply.github.com>
2026-06-01 17:58:30 +0800

    Create aaa.sh

---
+++ aaa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export PASS=$(cat pass.txt)
+GITLAB_URL="https://gitlab.com/dtiven13
+TOKEN="$PASS"
+PROJECT_ID="82751713"
+PROJECT_NAME="test6"
+NAMESPACE_ID="1"
+
+echo "Deleting project..."
+curl -s --request DELETE \
+  --header "PRIVATE-TOKEN: $TOKEN" \
+  "$GITLAB_URL/api/v4/projects/$PROJECT_ID"
+
+sleep 10
+
+echo "Creating project..."
+curl -s --request POST \
+  --header "PRIVATE-TOKEN: $TOKEN" \
+  --data "name=$PROJECT_NAME" \
+  --data "namespace_id=$NAMESPACE_ID" \
+  "$GITLAB_URL/api/v4/projects"
