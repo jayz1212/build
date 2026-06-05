e8499d22d483db75b750f8391bae6097148808e9
Lukman Simbolon <160797500+jayz1212@users.noreply.github.com>
2026-06-06 07:13:24 +0800

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -20,7 +20,7 @@ git clone https://github.com/jayz1212/local --depth 1 -b cd16 .repo/local_manife
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh
 export TARGET_USES_MINI_GAPPS=true
-export TARGET_USES_PICO_GAPPS=true
+#export TARGET_USES_PICO_GAPPS=true
 export TARGET_ENABLE_BLUR=false
 export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GMS=false
@@ -28,7 +28,7 @@ export TARGET_PERMISSIVE=true
 source build/envsetup.sh
 lunch lineage_blossom-bp4a-eng
 make installclean
-make clean # one time
+#make clean # one time
 #m bacon
 m evolution
 ccache -s
