0aef270212151105e3b5e4d6591e2d4a620d33c4
Lukman Simbolon <160797500+jayz1212@users.noreply.github.com>
2026-06-30 17:59:45 +0800

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -3,39 +3,12 @@ sudo apt install patchelf -y
 
 rm -rf .repo/local_manifests/
 
-
-#rm -rf hardware/dolby
-#rm -rf packages/apps/Settings
-#rm -rf packages/apps/Evolver
-#rm -rf packages/apps/Evolver
-# rm -rf bootable
-# rm -rf build/make
-# rm -rf frameworks/av
-#rm -rf frameworks/base
-# rm -rf hardware/google/pixel
-# rm -rf hawrdware/interfaces
-# rm -rf packages/modules/Bluetooth
-#rm -rf vendor
-#rm -rf TMP_PATCHES
-
-
-
 #repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --depth=1 --git-lfs
 #repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth=1 --git-lfs
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs --depth=1
 git clone https://github.com/jayz1212/local --depth 1 -b lg .repo/local_manifests
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
-
-
 /opt/crave/resync.sh
-
-
-
-
-# rm -rf hardware/mediatek/interfaces/hardware/bluetooth
-# rg -l -0 '<<<<<<<|=======|>>>>>>>' hardware/mediatek | xargs -0 sed -i '/^<<<<<<< /d;/^=======/d;/^>>>>>>> /d'
-# #./device/xiaomi/blossom/applyPatches.sh device/xiaomi/blossom/patches
-
 # export TARGET_USES_PICO_GAPPS=true
 # export TARGET_ENABLE_BLUR=false
 # export WITH_ADB_INSECURE=true
@@ -43,11 +16,9 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 # export WITH_GMS=false
 export CLANG_TARGET_ARM32="--target=arm-linux-android"
 source build/envsetup.sh
-#make clean
 
 
-#export WITH_GMS=false
-#rm -rf hardware/interfaces/biometrics/fingerprint/2.1/default
+
 
 
     
