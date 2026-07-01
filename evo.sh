826630a1bd1336757ad20fdd17f8b492b0fec8c3
Lukman Simbolon <160797500+jayz1212@users.noreply.github.com>
2026-07-01 17:31:08 +0800

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -1,13 +1,13 @@
 
 git clone https://github.com/xc112lg/rbe1 >/dev/null 2>&1
 
-source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe2.sh)  >/dev/null 2>&1
+
 
 rm -rf .repo/local_manifests/
 
 #repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --depth=1 --git-lfs
 #repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth=1 --git-lfs
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs --depth=1
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth=1
 git clone https://github.com/jayz1212/local --depth 1 -b lg .repo/local_manifests
 repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 /opt/crave/resync.sh
@@ -17,6 +17,7 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
 # export SELINUX_IGNORE_NEVERALLOWS=true
 # export WITH_GMS=false
 export CLANG_TARGET_ARM32="--target=arm-linux-android"
+source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe2.sh)  >/dev/null 2>&1
 source build/envsetup.sh
 
 
