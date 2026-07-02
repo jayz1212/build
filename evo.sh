a6da5fc6a89a95b341256a4fd7105bd44ac9ed9b
Lukman Simbolon <160797500+jayz1212@users.noreply.github.com>
2026-07-02 21:35:19 +0800

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -1,5 +1,5 @@
 
-git clone https://github.com/xc112lg/rbe1 >/dev/null 2>&1
+
 
 
 
@@ -7,10 +7,10 @@ rm -rf .repo/local_manifests/
 
 #repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --depth=1 --git-lfs
 #repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth=1 --git-lfs
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth=1
-git clone https://github.com/jayz1212/local --depth 1 -b lg .repo/local_manifests
-repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
-/opt/crave/resync.sh
+# repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth=1
+# git clone https://github.com/jayz1212/local --depth 1 -b lg .repo/local_manifests
+# repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
+# /opt/crave/resync.sh
 # export TARGET_USES_PICO_GAPPS=true
 # export TARGET_ENABLE_BLUR=false
 # export WITH_ADB_INSECURE=true
